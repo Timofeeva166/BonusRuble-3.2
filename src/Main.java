1321bc0f4bb818fa40142777d45ca5daebb507a5
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-06-15 15:35:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int InitAmount = 100;
+        int TopUpSum = 1100;
+
+        int BonusRuble;
+        if (TopUpSum > 1000) {
+            BonusRuble = TopUpSum / 100;
+        } else {
+            BonusRuble = 0;
+        }
+        int FinalAmount = InitAmount + TopUpSum + BonusRuble;
+
+        System.out.println("Итоговый счёт:" + FinalAmount);
+        System.out.println("Количество бонусных рублей:" + BonusRuble);
+    }
+}
